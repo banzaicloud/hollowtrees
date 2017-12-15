bcd912d3c1544e899cdf107a65261d13f8b25b64
Marton Sereg <martonsereg@gmail.com>
2017-12-15 17:06:38 +0100

    hack: do not exit

--- monitor/collector.go
+++ monitor/collector.go
@@ -81,38 +81,36 @@ func (c *Collector) Start() {
 		}
 	}()
 
-	go func() {
-		for {
-			select {
-			case <-reevaluatingTicker.C:
-				log.Info("ticker triggered:", time.Now())
-				vmPoolTasks, err := c.VmPoolManager.ReevaluateVmPools()
-				if err != nil {
-					log.Error("Failed to reevaluate VM pools in this tick: ", err)
-				}
-				if vmPoolTasks != nil {
-					for _, vmPool := range vmPoolTasks {
-						c.InProgress.Lock()
-						if !c.InProgress.r[*vmPool.VmPoolName] {
-							c.InProgress.r[*vmPool.VmPoolName] = true
-							c.Requests <- VmPoolRequest{VmPoolTask: vmPool}
-							log.WithFields(logrus.Fields{
-								"autoScalingGroup": *vmPool.VmPoolName,
-								"taskId":           vmPool.TaskID,
-								"action":           *vmPool.VmPoolAction,
-							}).Info("Pushing VM pool task to processor queue")
-						} else {
-							log.WithFields(logrus.Fields{
-								"autoScalingGroup": *vmPool.VmPoolName,
-								"taskId":           vmPool.TaskID,
-								"action":           *vmPool.VmPoolAction,
-							}).Info("A processor is already working on this VM pool")
-						}
-						c.InProgress.Unlock()
-					}
-				}
-				log.Info("ticker finished:", time.Now())
+	for {
+		select {
+		case <-reevaluatingTicker.C:
+			log.Info("ticker triggered:", time.Now())
+			vmPoolTasks, err := c.VmPoolManager.ReevaluateVmPools()
+			if err != nil {
+				log.Error("Failed to reevaluate VM pools in this tick: ", err)
 			}
+			if vmPoolTasks != nil {
+				for _, vmPool := range vmPoolTasks {
+					c.InProgress.Lock()
+					if !c.InProgress.r[*vmPool.VmPoolName] {
+						c.InProgress.r[*vmPool.VmPoolName] = true
+						c.Requests <- VmPoolRequest{VmPoolTask: vmPool}
+						log.WithFields(logrus.Fields{
+							"autoScalingGroup": *vmPool.VmPoolName,
+							"taskId":           vmPool.TaskID,
+							"action":           *vmPool.VmPoolAction,
+						}).Info("Pushing VM pool task to processor queue")
+					} else {
+						log.WithFields(logrus.Fields{
+							"autoScalingGroup": *vmPool.VmPoolName,
+							"taskId":           vmPool.TaskID,
+							"action":           *vmPool.VmPoolAction,
+						}).Info("A processor is already working on this VM pool")
+					}
+					c.InProgress.Unlock()
+				}
+			}
+			log.Info("ticker finished:", time.Now())
 		}
-	}()
+	}
 }
